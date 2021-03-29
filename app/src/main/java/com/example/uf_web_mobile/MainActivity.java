45299100bf2f9c6ba83a33ab94ec7474bb35cdeb
eleacarton <eleacarton@me.com>
2021-03-29 15:15:29 +0200

    Main et Home page

--- app/src/main/java/com/example/uf_web_mobile/MainActivity.java
+++ app/src/main/java/com/example/uf_web_mobile/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.uf_web_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Timer myTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myTimer = new Timer();
+        myTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // TODO : lancement de HomeActivity
+                Intent myIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        }, 2000);
     }
 }
